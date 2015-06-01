7cc7cdeb3ed50c0af3407708cdc120fe1cb1ea33
Al Williams <al.williams@awce.com>
2015-05-31 23:24:44 -0500

    Clean up on plot dialog. Fix printing example. Enhanced gnuplot screen examples.

--- plotdialog.cpp
+++ plotdialog.cpp
@@ -2,9 +2,35 @@
 #include "ui_plotdialog.h"
 #include <QSettings>
 
+const char *defcmds[] =
+{
+    "gnuplot {SCRIPT}",
+    "gnuplot {SCRIPT}",
+    "qtiplot {FILE}"
 
-const char defcmd[]="gnuplot {SCRIPT}";
-const char defscript[]="set terminal X11\nset datafile separator \",\"\nplot \"{FILE}\" using 1:2 with lines\npause -1";
+};
+
+const char *defscripts[]=
+{
+    "# Terminal x11 is basic\n"
+    "# Terminal qt has more features\n"
+    "# but only the most recent one works at one time\n"
+    "# You can change the size\n"
+    "xsize=800\n"
+    "ysize=600\n"
+    "set terminal qt 1 size xsize,ysize\n"
+    "set datafile separator \",\"\n"
+    "set title \"Channel 1\"\n"
+    "plot \"{FILE}\" using 1:2 with lines  title columnhead(2)  lc \"orange\"\n"
+    "# You can play with the window until you click the mouse\n"
+    "pause mouse\n"
+    "# Go on to channel 2\n"
+    "set title \"Channel 2\"\n"
+    "plot \"{FILE}\" using 1:3 with lines title columnhead(3) lc \"blue\"\n"
+    "pause mouse close # for some reason, now you'll have to close with the X on the window\n",
+    "set terminal postscript portrait enhanced mono\nset output '/tmp/scope.ps'\nset datafile separator \",\"\nplot \"{FILE}\" using 1:2 with lines\npause -1\n",
+    ""
+};
 
 PlotDialog::PlotDialog(QWidget *parent) :
     QDialog(parent),
@@ -12,8 +38,8 @@ PlotDialog::PlotDialog(QWidget *parent) :
 {
     QSettings set;
     ui->setupUi(this);
-    command=set.value("plot/command",defcmd).toString();
-    script=set.value("plot/script",defscript).toString();
+    command=set.value("plot/command",defcmds[0]).toString();
+    script=set.value("plot/script",defscripts[0]).toString();
     ui->plotscript->setPlainText(script);
     ui->plotcmd->setText(command);
 }
@@ -40,20 +66,7 @@ void PlotDialog::on_plotcmd_textChanged(const QString &arg1)
 }
 
 
-const char *defcmds[] =
-{
-    "gnuplot {SCRIPT}",
-    "gnuplot {SCRIPT}",
-    "qtiplot {FILE}"
 
-};
-
-const char *defscripts[]=
-{
-    "set terminal X11\nset datafile separator \",\"\nplot \"{FILE}\" using 1:2 with lines\npause -1\n",
-    "set terminal postscript portraint enhanced mono\nset output '/tmp/scope.ps'\n,set datafile separator \",\"\nplot \"{FILE}\" using 1:2 with lines\npause -1\n",
-    ""
-};
 
 
 void PlotDialog::on_comboBox_currentIndexChanged(int index)
